4054c3d349275afb8621e58cc3d51f585fc7f192
boston <bost56@gmail.com>
2012-03-27 01:20:49 +0600

    Слияние с наработками olvlvl

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
-##Шаблон для формирования документации Joostina 2.* API ##
+##Шаблон для формирования документации Joostina 2.* API
 
 Шаблон предназначен для системы генерации кода ApiGen ( https://github.com/apigen/apigen или http://apigen.org )
+
+### Использованы стили из проекта Bootswatch:
+	* http://bootswatch.com/
+	* https://github.com/thomaspark/bootswatch
 
 Актуальный пример рабочей документации: http://xboston.github.com/joostina-docs/index.html
 
 
-### Требования: ###
+### Требования:
 * php 5.3
 * ApiGen 2.6
--- js/main.js
+++ js/main.js
@@ -15,25 +15,28 @@ $(function() {
 	var $groups = $('#groups');
 
 	// Hide deep packages and namespaces
-	$('ul span', $groups).click(function() {
+	$('ul span', $groups).click(function(event) {
+		event.preventDefault();
+		event.stopPropagation();
 		$(this)
 			.toggleClass('collapsed')
-			.next('ul')
-				.toggleClass('collapsed');
+			.parent()
+				.next('ul')
+					.toggleClass('collapsed');
 	}).click();
 
 	$active = $('ul li.active', $groups);
 	if ($active.length > 0) {
 		// Open active
-		$('> span', $active).click();
+		$('> a > span', $active).click();
 	} else {
 		$main = $('> ul > li.main', $groups);
 		if ($main.length > 0) {
 			// Open first level of the main project
-			$('> span', $main).click();
+			$('> a > span', $main).click();
 		} else {
 			// Open first level of all
-			$('> ul > li > span', $groups).click();
+			$('> ul > li > a > span', $groups).click();
 		}
 	}
 
@@ -64,7 +67,7 @@ $(function() {
 			var location = window.location.href.split('/');
 			location.pop();
 			var parts = data[1].split(/::|$/);
-			var file = $.sprintf(ApiGen.config.templates.main[autocompleteFiles[data[0]]].filename, parts[0].replace('\\', '.').replace(/[^\w\.]/g, ''));
+			var file = $.sprintf(ApiGen.config.templates.main[autocompleteFiles[data[0]]].filename, parts[0].replace(/\\/g, '.').replace(/[^\w\.]/g, ''));
 			if (parts[1]) {
 				file += '#' + parts[1].replace(/([\w]+)\(\)/, '_$1');
 			}
